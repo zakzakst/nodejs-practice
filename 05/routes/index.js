b5f10cca88e15bbf389a1c5c14442754f8de9255
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-12-10 19:04:02 +0900

    伝言板の作成

--- 05/routes/index.js
+++ 05/routes/index.js
@@ -1,9 +1,83 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'my-nodeapp-db',
+    charset: 'utf8',
+  },
+});
+var Bookshelf = require('bookshelf')(knex);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Bookshelf.plugin('pagination');
+const User = Bookshelf.Model.extend({
+  tableName: 'users',
+});
+const Message = Bookshelf.Model.extend({
+  tableName: 'message',
+  hasTimestamps: true,
+  user: () => {
+    return this.belongsTo(User);
+  },
+});
+
+router.get('/', (req, res, next) => {
+  console.log(req.session.login);
+  if (req.session.login === undefined) {
+    console.log('/users');
+    res.redirect('/users');
+  } else {
+    console.log('/1');
+    // res.redirect('/1');
+  }
+});
+
+// router.get('/:page', (req, res, next) => {
+//   if (req.session.login === undefined) {
+//     res.redirect('/users');
+//     return;
+//   }
+//   const pg = Number(req.params.page);
+//   if (pg < 1) {pg = 1;}
+//   new Message()
+//     .orderBy('created_at', 'DESC')
+//     .fetchPage({
+//       page: pg,
+//       pageSize: 10,
+//       withRelated: ['user'],
+//     })
+//     .then(collection => {
+//       res.render('index', {
+//         title: 'Board',
+//         login: req.session.login,
+//         collection: collection.toArray(),
+//         pagination: collection.pagination,
+//       });
+//     })
+//     .catch(err => {
+//       res.status(500).json({
+//         error: true,
+//         data: {
+//           message: err.message,
+//         },
+//       });
+//     });
+// });
+
+router.post('/', (req, res, next) => {
+  const rec = {
+    message: req.body.msg,
+    user_id: req.session.login.id,
+  };
+  new Message(rec)
+    .save()
+    .then(model => {
+      res.redirect('/');
+    });
 });
 
 module.exports = router;
--- 05/routes/users.js
+++ 05/routes/users.js
@@ -1,9 +1,80 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'my-nodeapp-db',
+    charset: 'utf8',
+  },
+});
+var Bookshelf = require('bookshelf')(knex);
+var User = Bookshelf.Model.extend({
+  tableName: 'users',
+});
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  res.render('users/login', {
+    title: 'Users/Login',
+    form: {
+      name: '',
+      password: '',
+    },
+    content: '名前とパスワードを入力してください。',
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const name = req.body.name;
+  const password = req.body.password;
+  User
+    .query({
+      where: {
+        name: name,
+      },
+      andWhere: {
+        password: password,
+      },
+    })
+    .fetch()
+    .then(model => {
+      if (model === null) {
+        res.render('users/login', {
+          title: '再入力',
+          content: '名前またはパスワードが違います。',
+          form: req.body,
+        });
+      } else {
+        req.session.login = model.attributes;
+        res.render('users/login', {
+          title: 'Users/Login',
+          content: 'ログインしました。トップページに戻ってメッセージを送信ください。',
+          form: req.body,
+        });
+      }
+    })
+});
+
+router.get('/add', (req, res, next) => {
+  res.render('users/add', {
+    title: 'Users/Add',
+    form: {
+      name: '',
+      password: '',
+      comment: '',
+    },
+    content: '※登録する名前・パスワード・コメントを入力してください。',
+  });
+});
+
+router.post('/add', (req, res, next) => {
+  req.session.login = null;
+  new User(req.body).save().then(model => {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
