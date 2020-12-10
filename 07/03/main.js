1375cb5b222ef2061b46943cbf14a78620e73b19
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-12-11 06:19:40 +0900

    練習7-3

---
+++ 07/03/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// const cities = require('cities');
+// const myCity = cities.zip_lookup('10016');
+// console.log(myCity);
+
+const addNum = require('./addNum');
+const result = addNum.addNum(1, 2);
+console.log(result);
---
+++ 07/03/printMessages.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const messagesModule = require('./messages');
+messagesModule.messages.forEach(m => console.log(m));
